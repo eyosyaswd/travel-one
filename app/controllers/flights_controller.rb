87689b5c74d9f411b4a7301816bf591535762604
Will Theuer <watheuer@email.wm.edu>
2016-02-27 18:36:05 -0500

    Working sabre api

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,10 +2,22 @@ class FlightsController < ApplicationController
   before_action :authenticate_user!
 
   def initialize
-    @sab = Sabre.new(ENV["SB_ID"], ENV["SB_SC"])
+    clientid = Base64.encode64(ENV["SB_ID"]).gsub("\n", '')
+    secret = Base64.encode64(ENV["SB_SC"]).gsub("\n", '')
+    @client = OAuth2::Client.new(clientid, secret, site: 'https://api.test.sabre.com', token_url: '/v1/auth/token')
   end
 
   def index
-    render json: @sab.destination_finder(params[:orig], params[:departure], params[:retdate])
+    options = {
+      origin: params[:orig],
+      departuredate: params[:departure],
+      returndate: params[:retdate],
+      maxfare: params[:maxfare],
+      topdestinations: 10
+    }
+    token = @client.client_credentials.get_token
+
+    response = token.get('/v2/shop/flights/fares', params: options)
+    render json: response.body
   end
 end
