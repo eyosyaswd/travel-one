5392ad060b5e015ee0d90459fae8f225c76e3e8d
Will Theuer <watheuer@email.wm.edu>
2016-02-27 00:58:56 -0500

    Added vacation controller

---
+++ app/controllers/vacation_controller.rb
@@ -0,0 +1,48 @@
+class VacationController < ApplicationController
+  def index
+    render json: Vacation.where(user: current_user)
+  end
+
+  def create
+    vacation = Vacation.new filtered_params
+    vacation.user = current_user
+
+    if vacation.valid?
+      vacation.save!
+      current_user.vacations += [vacation]
+      current_user.save!
+    else
+      head :error
+      return
+    end
+
+    render json: vacation
+  end
+
+  def show
+    vacation = Vacation.find_by(id: params[:id], user_id: current_user.id)
+
+    if vacation.nil?
+      head :error
+    else
+      render json: vacation
+    end
+  end
+
+  def destroy
+    # only destroy user's vacations
+    vacation = Vacation.find_by(id: params[:id], user_id: current_user.id)
+
+    if vacation.nil?
+      head :error
+    else
+      vacation.destroy
+      render json: vacation
+    end
+  end
+
+  private
+    def filtered_params
+      params.require(:vacation).permit(:id, :origin, :destination, :fare, :departure_time, :return_time)
+    end
+end
--- app/models/vacation.rb
+++ app/models/vacation.rb
@@ -2,9 +2,5 @@ class Vacation < ActiveRecord::Base
   belongs_to :user
 
   # need origin, fare, origin and return time
-  validates :origin, presence: true
-  validates :destination, presence: true
-  validates :fare, presence: true
-  validates :departure_time, presence: true
-  validates :return_time, presence: true
+  validates :origin, :destination, :fare, :departure_time, :return_time, presence: true
 end
