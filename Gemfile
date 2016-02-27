c323ee1dc19c9efc3c125ae3ceaa07e7cab4815c
Will Theuer <watheuer@email.wm.edu>
2016-02-27 01:56:02 -0500

    Fixed authentication

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Devise authentication
 gem 'devise'
+gem 'simple_token_authentication'
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160227043258) do
+ActiveRecord::Schema.define(version: 20160227065133) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -27,8 +27,10 @@ ActiveRecord::Schema.define(version: 20160227043258) do
     t.string   "capitalone_id"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "authentication_token"
   end
 
+  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token"
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
