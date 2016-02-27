1fe0882f187d5e0f79884c5ddecef431b0677b12
Will Theuer <watheuer@email.wm.edu>
2016-02-27 09:59:28 -0500

    window to CapitalOne api

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,3 +1,17 @@
 class AccountsController < ApplicationController
-  
+  def initialize
+    @cap1 = CapitalOne.new(ENV["CAP1_KEY"])
+  end
+
+  def index
+    render json: @cap1.accounts(current_user.capitalone_id).parsed_response
+  end
+
+  def show
+    render json: @cap1.account(params[:id])
+  end
+
+  def create
+    render json: @cap1.create_account(current_user.capitalone_id, params[:nickname])
+  end
 end
